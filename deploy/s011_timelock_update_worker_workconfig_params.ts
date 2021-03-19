39b637b2b837244b76851e57eee9966c6dd8d146
spicysquid168 <spicysquid168@gmail.com>
2021-03-19 11:00:57 +0700

    chore: move acceptDebt to UPDATES

--- deploy/s011_timelock_update_worker_workconfig_params.ts
+++ deploy/s011_timelock_update_worker_workconfig_params.ts
@@ -18,26 +18,31 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const UPDATES = [{
     WORKER_ADDRESS: '0xc2440b08478ce53351176d37FD63724f57961197',
+    ACCEPT_DEBT: true,
     WORK_FACTOR: '6000',
     KILL_FACTOR: '8000',
     MAX_PRICE_DIFF: '13000',
   }, {
     WORKER_ADDRESS: '0x6Ac6619BbA455BD247776f4BbE767Fb516E19D18',
+    ACCEPT_DEBT: true,
     WORK_FACTOR: '6667',
     KILL_FACTOR: '8333',
     MAX_PRICE_DIFF: '13000',
   }, {
     WORKER_ADDRESS: '0xe7aa7b1c8317203cb5b52a7002DFF489a7086157',
+    ACCEPT_DEBT: true,
     WORK_FACTOR: '6667',
     KILL_FACTOR: '8333',
     MAX_PRICE_DIFF: '13000',
   }, {
     WORKER_ADDRESS: '0x0d0875fce644De95c2ae7f0E4D4596b2A8dC43e3',
+    ACCEPT_DEBT: true,
     WORK_FACTOR: '8333',
     KILL_FACTOR: '9000',
     MAX_PRICE_DIFF: '13000',
   }, {
     WORKER_ADDRESS: '0xF3ECC0e5c238C7082fC59e682104DEA2f49A3787',
+    ACCEPT_DEBT: true,
     WORK_FACTOR: '6667',
     KILL_FACTOR: '8333',
     MAX_PRICE_DIFF: '13000',
@@ -66,7 +71,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       ethers.utils.defaultAbiCoder.encode(
         ['address[]','(bool acceptDebt,uint64 workFactor,uint64 killFactor,uint64 maxPriceDiff)[]'],
         [
-          [UPDATES[i].WORKER_ADDRESS], [{acceptDebt: true, workFactor: UPDATES[i].WORK_FACTOR, killFactor: UPDATES[i].KILL_FACTOR, maxPriceDiff: UPDATES[i].MAX_PRICE_DIFF}]
+          [UPDATES[i].WORKER_ADDRESS], [{acceptDebt: UPDATES[i].ACCEPT_DEBT, workFactor: UPDATES[i].WORK_FACTOR, killFactor: UPDATES[i].KILL_FACTOR, maxPriceDiff: UPDATES[i].MAX_PRICE_DIFF}]
         ]
       ), EXACT_ETA
     );
