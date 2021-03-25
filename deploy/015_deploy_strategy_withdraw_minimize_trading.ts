96e18f17a3764f638108a04facbb92864b42d591
spicysquid168 <spicysquid168@gmail.com>
2021-03-25 17:14:04 +0700

    deploy: finished StrategyMinimizeTrading to testnet

--- deploy/015_deploy_strategy_withdraw_minimize_trading.ts
+++ deploy/015_deploy_strategy_withdraw_minimize_trading.ts
@@ -1,10 +1,10 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
-import { ethers } from 'hardhat';
-import { Timelock__factory } from '../typechain'
+import { ethers, upgrades } from 'hardhat';
+import { StrategyAddBaseTokenOnly__factory, StrategyLiquidate__factory, StrategyWithdrawMinimizeTrading__factory, Timelock__factory } from '../typechain';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  /*
+    /*
   ░██╗░░░░░░░██╗░█████╗░██████╗░███╗░░██╗██╗███╗░░██╗░██████╗░
   ░██║░░██╗░░██║██╔══██╗██╔══██╗████╗░██║██║████╗░██║██╔════╝░
   ░╚██╗████╗██╔╝███████║██████╔╝██╔██╗██║██║██╔██╗██║██║░░██╗░
@@ -14,17 +14,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   Check all variables below before execute the deployment script
   */
 
-  const STRATEGY = '0x53d9180C9c8E09D4e75C47f49882b55186efcc37'
-  const WORKERS = [
-    '0x2c12663339Fdce1A3088C0245FC2499255ccF7eC',
-    '0xf20E73385397284f37c47963E2515156fCf33360',
-    '0xA950ee51Ac3b27a1a6C87D6448D6717ACBc7b0A8',
-    '0xDf605784D78D42b5507Be14D6533a835E2692A16',
-    '0x00d078c7e058bA5106878b9e0CC09Ef1BF510925'
-  ];
-
-  const TIMELOCK = '0xb3c3aE82358DF7fC0bd98629D5ed91767e45c337';
-  const EXACT_ETA = '1616667060';
+  const ROUTER = '0xf46A02489B99C5A4a5cC31AA3F9eBD6A501D4B49';
 
 
 
@@ -35,26 +25,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 
 
-
-  const timelock = Timelock__factory.connect(TIMELOCK, (await ethers.getSigners())[0]);
-
-  for(let i = 0; i < WORKERS.length; i++) {
-    console.log(">> Timelock: Setting okStrats via Timelock");
-    await timelock.queueTransaction(
-      WORKERS[i], '0',
-      'setStrategyOk(address[],bool)',
-      ethers.utils.defaultAbiCoder.encode(
-        ['address[]','bool'],
-        [
-          [STRATEGY], true
-        ]
-      ), EXACT_ETA
-    );
-    console.log("generate timelock.executeTransaction:")
-    console.log(`await timelock.executeTransaction('${WORKERS[i]}', '0', 'setStrategyOk(address[],bool)', ethers.utils.defaultAbiCoder.encode(['address[]','bool'],[['${STRATEGY}'], true]), ${EXACT_ETA})`)
-    console.log("✅ Done");
-  }
+  console.log(">> Deploying an upgradable StrategyWithdrawMinimize contract");
+  const StrategyWithdrawMinimizeTrading = (await ethers.getContractFactory(
+    "StrategyWithdrawMinimizeTrading",
+    (await ethers.getSigners())[0],
+  )) as StrategyWithdrawMinimizeTrading__factory;
+  const strategyWithdrawMinimizeTrading = await upgrades.deployProxy(StrategyWithdrawMinimizeTrading, [ROUTER]);
+  await strategyWithdrawMinimizeTrading.deployed()
+  console.log(`>> Deployed at ${strategyWithdrawMinimizeTrading.address}`);
 };
 
 export default func;
-func.tags = ['TimelockUpdateAddStratWorkers'];
+func.tags = ['StrategyWithdrawMinimizeTrading'];
